0f85672ba7f9c1dd1d2c4261acaa1496fa8caa1a
Balint Radnai <radnai.balint@edu.bme.hu>
2021-03-08 23:58:35 +0100

    Add Tachograph module

---
+++ tachograph/src/main/java/hu/bme/mit/train/controller/Tachograph.java
@@ -0,0 +1,11 @@
+package hu.bme.mit.train.controller;
+
+import com.google.common.collect.HashBasedTable;
+
+public class Tachograph {
+
+	private HashBasedTable<Integer, String, Integer> table;
+
+
+
+}
